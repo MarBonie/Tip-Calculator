7d9718eb7c0e184b82e9020a7f6b6ee8d2f20df3
MarBonie <barcin.boniecki@gmail.com>
2023-02-06 23:33:31 +0100

    js working logic of displaying tip/total ammount

--- script.js
+++ script.js
@@ -2,29 +2,20 @@
 let billValue = document.querySelector('.billvalue');
 let numberofpeople = document.querySelector('.numberofpeople');
 let button__reset = document.querySelector('.button__reset');
-let tipperson = document.querySelector('.tippreson');
+let tipperson = document.querySelector('.tipperson');
 let totalperson = document.querySelector('.totalperson');
-let button__percentage = document.querySelector('.button__percentage');
+let button__percentage = document.querySelectorAll('.button__percentage');
 
+// Variable to contain current % of tip
+let CurrentPercenateTip = [];
 
-//Current tip percentage
-let currentTipPercentage = [];
+button__percentage.forEach(element => element.addEventListener("click", () => {
+    tipperson.innerHTML = ((billValue.value / numberofpeople.value) * 0.01 * element.value).toFixed(2);
+    totalperson.innerHTML = (Number((billValue.value / numberofpeople.value)) + Number(tipperson.innerHTML)).toFixed(2);
+}));
 
-button__percentage.addEventListener("click", () => {
-    currentTipPercentage = button__percentage.value;
-})
 
 button__reset.addEventListener("click", () => {
-
-    numberofpeople.value = 1;
-
-    billValue.value = currentTipPercentage;
+    numberofpeople.value = 0;
+    billValue.value = 0;
 })
-
-function calculate() {
-    if (billValue.value && numberofpeople.value) {
-        return tipperson.value = (billValue.value / numberofpeople.value);
-    }
-}
-
-totalperson.innerHTML = 5344;
